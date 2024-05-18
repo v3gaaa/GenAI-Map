49ad964196d85e3c031aeeb6b3a948f4326030fb
v3gaaa <svsm03@hotmail.com>
2024-05-17 19:10:41 -0600

    Agregue mejor formato de directorios a la parte de backend

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const {Pool} = require('pg');
+
+const db = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+module.exports = db;
--- backend/main.js
+++ backend/main.js
@@ -2,42 +2,32 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
-const { Pool } = require('pg');
+
+// Se importa la configuración de la base de datos
+const db = require('./config/db');
 
 // Inicio de la aplicación
 const app = express();
 const port = 8000;
 
-// Configuración de la base de datos
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
 // Middlewares (Plug-ins del servidor)
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Se crea un endpoint para probar el servidor
 app.get("/api/testdb", async (req, res) => {
     try {
-        const client = await pool.connect();
-        await client.query('SELECT NOW()');
-        client.release();
+        //Test connection to the db
+        const result = await db.query("SELECT NOW()");
         res.json({
-            message: "Conexion con la base de datos funcionando correctamente",
-            dbConnection: "Success",
+            message: "Successful connection to the database",
+            data: result.rows[0],
         });
     } catch (error) {
-        res.status(500).json({
-            message: "Error al conectar con la base de datos",
-            dbConnection: "Failed",
+        res.json({
+            message: "Error connecting to the database",
+            error: error.message,
         });
     }
 });
