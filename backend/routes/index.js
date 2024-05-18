d36893e1d5670f898ab6864a08cd8350aa0d14cf
v3gaaa <svsm03@hotmail.com>
2024-05-17 19:56:01 -0600

    Agregado endpoints para manejar operaciones CRUD de la tabla AIs de la base de datos

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const testdbRoutes = require('./testdb');
+const aiRoutes = require('./ai');
 
 router.use('/testdb', testdbRoutes);
+router.use('/ais', aiRoutes);
 
 router.get("/", (req, res) => {
     res.json({ message: "API funcionando correctamente" });
