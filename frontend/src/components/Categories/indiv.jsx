801738bd3862ff6f9ea38b699ddc829ee263f010
gusortepz <A01637220@tec.mx>
2024-05-31 18:40:22 -0600

    Ultima version

--- frontend/src/components/Categories/indiv.jsx
+++ frontend/src/components/Categories/indiv.jsx
@@ -4,6 +4,7 @@ import "./indiv.css";
 import Planet2Container from "../planetcomponents/Planet2Container";
 import CatCarousel from "./catCarousel";
 import Planet from "../planetcomponents/Planet";
+import Rankings from "./rankings";
 
 const slides = [
   { name: 'OpenAI', title: 'ChatGPT', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/1200px-ChatGPT_logo.svg.png' },
@@ -24,28 +25,19 @@ const Indiv = ({Category}) => {
                 <div>
                     <p className="text-l text-white pb-5">ChatGPT es un modelo de lenguaje basado en la arquitectura GPT (Generative Pre-trained Transformer) desarrollado por OpenAI. Es parte de una familia de modelos de inteligencia artificial (IA) diseñados para generar texto en lenguaje natural. Aquí tienes un resumen de su tecnología y funcionalidad:</p>
                 </div>
+                <Rankings />
             </div>
 
             <div className="container-down">
               <div className="down">
                 <div className="down-1">
-                  <div>
-                    <h2 className="text-white text-xl ml-[-60%]">Anterior</h2>
-                  </div>
-                  <div className="down-1-in hover:scale-105">
-                    <Planet surface={Category} />
-                  </div>
+                  <RocketButton text={"Recursos"} />
                 </div>
-                <div className="down-2 w-3/5 h-auto aspect-square top-1/4 transition transform hover:-translate-y-1 hover:scale-105">
-                  <Planet surface={Category} />
+                <div className="downn-2">
+                  <RocketButton text={"Proyectos"} />
                 </div>
                 <div className="down-3">
-                  <div>
-                  <h2 className="text-white text-xl mr-[-60%]">Siguiente</h2>
-                  </div>
-                  <div className="down-3-in hover:scale-105">
-                    <Planet surface={Category} />
-                  </div>
+                  <RocketButton text={"Docs"} />
                 </div>
               </div>
                 
